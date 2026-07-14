035715a618b091430b67e83c59fd0c36393215de
Derese4803 <derese.esubalew-c@oneacrefund.org>
2026-07-14 17:02:10 +0300

    Update login credentials for admin user

--- auth.py
+++ auth.py
@@ -1,3 +1,3 @@
 def login_user(username, password):
     # You can replace this with database logic later
-    return username == "admin" and password == "oaf2026"
+    return username == "admin" and password == "admin123"
